4e0a6e8a4f8826b2592f01919a0769941a98636a
Pramod <pramod.h@codilar.com>
2023-07-08 19:26:13 +0530

    text gold

--- mail.php
+++ mail.php
@@ -1,18 +1,42 @@
 <?php
-    $myemail = "pramodsh582@gmail.com";
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the form data
+    $appointmentType = $_POST['ap-type'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
     $name = $_POST['your-name'];
-    $email_address = $_POST["your-email"];
+    $number = $_POST['your-number'];
+    $email = $_POST['your-email'];
+    $priceRange = $_POST['price-range'];
     $message = $_POST['your-message'];
-    $phone = $_POST['your-number'];
-    $price = $_POST['price-range'];
-    $to = $myemail;
-    $email_subject = "$name";
-    $email_body = "You have received a new message. ".
-    " Here are the details:\n Name: $name \n ".
-    "Email: $email_address\n Phone: $phone\n Message \n $message";
-    $headers = "From: $myemail\n";
-    $headers = "Reply-To: $email_address";
-    mail($to,$email_subject,$email_body,$headers);
-    //redirect to the 'thank you' page
-    header('Location: ./index.html');
-?>
+
+    // Create the email message
+    $to = 'pramodsh582@gmail.com'; // Enter your email address here
+    $subject = 'New Design Consultation Request';
+
+    $emailMessage = "Appointment Type: $appointmentType\n";
+    $emailMessage .= "Date: $date\n";
+    $emailMessage .= "Time: $time\n";
+    $emailMessage .= "Name: $name\n";
+    $emailMessage .= "Number: $number\n";
+    $emailMessage .= "Email: $email\n";
+    $emailMessage .= "Price Range: $priceRange\n";
+    $emailMessage .= "Message:\n$message";
+
+    // Set the headers
+    $headers = "From: $name <$email>" . "\r\n";
+    $headers .= "Reply-To: $email" . "\r\n";
+
+    // Send the email
+    if (mail($to, $subject, $emailMessage, $headers)) {
+        echo 'Thank you for your message. We will get back to you soon.';
+    } else {
+        echo 'Unable to send the email. Please try again later.';
+    }
+} else {
+    // Redirect to the form page if accessed directly without submitting the form
+    header('Location: index.html');
+    exit;
+}
